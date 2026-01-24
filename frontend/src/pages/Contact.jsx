33263ff5b2c7a514f0432213ea0ff83a3c1e24ba
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-01-24 06:05:47 +0530

    created contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Contact = () => {
   return (
     <div>
-        
+        <div className='text-center text-2xl pr-19 text-gray-500'>
+          <p>CONTACT <span>US</span></p>
+        </div>
+
+        <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+          <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+
+          <div className='flex flex-col justify-center items-start gap-6'>
+            <p className='font-semibold text-lg text-gray-600'>Our OFFICE</p>
+            <p className='text-gray-500'>Scheme no. 78, Vijay Nagar <br /> Indore, Madhya Pradesh, INDIA</p>
+            <p className='text-gray-500'>Tel: 91+ 9999988888 <br /> Email: vipinsingh.cse@gmail.com</p>
+            <p className='font-semibold text-lg text-gray-600'>Careers at PRESCRIPTO</p>
+            <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+            <button className='border border-black px-8 py-4 tex
+            t-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+          </div>
+        </div>
     </div>
   )
 }
