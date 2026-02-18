a21d6e3431e7104f338e7a6657b6efe1934564d7
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-02-18 05:50:36 +0530

    Created Patient Login and Registration system

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,12 +8,10 @@ const AppContextProvider = (props) => {
 
     const currencySymbol = '$'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const [doctors, setDoctors] = useState([])
 
-    const value = {
-        doctors,
-        currencySymbol
-    }
+    const [doctors, setDoctors] = useState([])
+    const [token,setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
+
 
     const getDoctorsData = async()=> {
         try {
@@ -29,6 +27,14 @@ const AppContextProvider = (props) => {
 
             
         }
+    }
+
+
+    const value = {
+        doctors,
+        currencySymbol,
+        token,setToken, 
+        backendUrl
     }
 
     useEffect(()=>{
