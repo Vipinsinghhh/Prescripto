8f473f72dccd882805d30e63645074059e980b2e
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-02-21 08:32:18 +0530

    Displayed Booked appointments on Website

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -181,4 +181,19 @@ const bookAppointment = async (req, res) => {
     }
 }
 
-export { registerUser, loginUser, getProfile, updateProfile, bookAppointment };
+// API to get user appointments for frontend my-appointment page
+
+const listAppointment = async (req,res) => {
+
+    try {
+        
+        const userId = req.userId
+        const appointments = await appointmentModel.find({userId})
+        return res.json({success:true,appointments})
+    } catch (error) {
+         console.log(error);
+        return res.json({ success: false, message: error.message });
+    }
+}
+
+export { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment };
