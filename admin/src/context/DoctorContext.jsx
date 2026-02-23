4eb0c88e9458b6cbdd0e96620f99da7051ed384d
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-02-23 11:50:07 +0530

    Displayed and Updated profile data In Doctor panel

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -12,6 +12,7 @@ const DoctorContextProvider = (props) => {
     const [dToken, setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : '')
     const [appointments, setAppointments] = useState([])
     const [dashData, setDashData] = useState(false)
+    const [profileData, setProfileData] = useState(false)
 
     const getAppointments = async () => {
         try {
@@ -84,6 +85,22 @@ const DoctorContextProvider = (props) => {
         }
     }
 
+    const getProfileData = async ()=> {
+        try {
+            
+            const {data} = await axios.get(backendUrl + '/api/doctor/profile',{headers:{dToken}})
+            if (data.success) {
+                setProfileData(data.profileData)
+                console.log(data.profileData)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         dToken, setDToken,
         backendUrl,
@@ -91,7 +108,8 @@ const DoctorContextProvider = (props) => {
         setAppointments,
         getAppointments,
         completeAppointment, cancelAppointment,
-        dashData,setDashData,getDashData
+        dashData,setDashData,getDashData,
+        profileData,setProfileData,getProfileData,
 
     }
 
